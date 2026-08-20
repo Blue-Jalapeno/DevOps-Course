20ca2ea7f3ebb39d23eca7de90fc05fd868d793f
Blue-Jalapeno <github.jalapeno749@passmail.com>
2026-08-19 20:42:24 -0700

    Initial commit

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def main():
+    return "Hello and welcome to the ThinkNyx DevOps project!"
+
+@app.route('/services')
+def services():
+    return "Here are our services!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
